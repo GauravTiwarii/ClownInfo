b11598092b6e0cb1e3976f132f429581dead3735
Love <Love@love.local>
2016-08-21 20:20:49 +0530

    Initial Commit

---
+++ ClownInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClownInfo
+//
+//  Created by Love on 8/21/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
